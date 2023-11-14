f5ff7777eba6e61af36a05de862edc5e81c11768
NotRhea <143529100+NotRhea@users.noreply.github.com>
2023-11-13 22:20:20 -0500

    Add files via upload

---
+++ harmonic-collection/entry-9/app.js
@@ -0,0 +1,32 @@
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+var card2 = document.querySelector('.card2');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+
+var card3 = document.querySelector('.card3');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+
+var card4 = document.querySelector('.card4');
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+
+var card5 = document.querySelector('.card5');
+card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+});
+
+var card6 = document.querySelector('.card6');
+card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+});
+
+
+
